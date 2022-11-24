ea0915fe9694a56bcf44acb66778f9c000a82eb2
thadeus <thadeusnyariki94@gmail.com>
2022-11-24 08:07:01 +0300

    Done assignment

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -16,4 +16,12 @@ class BirdsController < ApplicationController
     end
   end
 
+  # POST /birds
+  def create
+    bird = Bird.create(name: params[:name], species: params[:species])
+    render json: bird, status: :created
+  end
+
+  # etc
+
 end
